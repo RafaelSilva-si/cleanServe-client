6be541505265a7e23931dd8c58eca26e8028bc62
RafaelSilva-si <rafael.silva@digi.ag>
2023-08-27 11:50:06 -0300

    feat: adiciona rota de dashboard na raiz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { BudgetModule } from './modules/budget/budget.module';
 import { ClientModule } from './modules/client/client.module';
 import { ServiceModule } from './modules/service/service.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 const routes: Routes = [
+  { path: 'dashboard', loadChildren: () => DashboardModule },
   { path: 'budget', loadChildren: () => BudgetModule },
   { path: 'client', loadChildren: () => ClientModule },
   { path: 'service', loadChildren: () => ServiceModule },
