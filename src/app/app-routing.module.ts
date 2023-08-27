9ca86146942f36e4bf9b5e77f0d3b46beb6ba148
RafaelSilva-si <rafael.silva@digi.ag>
2023-08-27 12:02:45 -0300

    feat: adiciona rota de schedule na raiz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { BudgetModule } from './modules/budget/budget.module';
 import { ClientModule } from './modules/client/client.module';
 import { ServiceModule } from './modules/service/service.module';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { ScheduleModule } from './modules/schedule/schedule.module';
 
 const routes: Routes = [
   { path: 'dashboard', loadChildren: () => DashboardModule },
   { path: 'budget', loadChildren: () => BudgetModule },
   { path: 'client', loadChildren: () => ClientModule },
   { path: 'service', loadChildren: () => ServiceModule },
+  { path: 'schedule', loadChildren: () => ScheduleModule },
 ];
 
 @NgModule({
