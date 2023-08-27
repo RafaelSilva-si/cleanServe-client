232b3f75e88a2dc76df3e6d2f2225bff8c4cb20c
RafaelSilva-si <rafael.silva@digi.ag>
2023-08-27 11:41:59 -0300

    refactor: referencia services nas rotas principais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BudgetModule } from './modules/budget/budget.module';
 import { ClientModule } from './modules/client/client.module';
+import { ServiceModule } from './modules/service/service.module';
 
 const routes: Routes = [
   { path: 'budget', loadChildren: () => BudgetModule },
   { path: 'client', loadChildren: () => ClientModule },
+  { path: 'service', loadChildren: () => ServiceModule },
 ];
 
 @NgModule({
