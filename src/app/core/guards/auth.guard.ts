3511a4edbcb2242740c1f962612b771a8870e653
RafaelSilva-si <rafael.silva@digi.ag>
2023-08-31 08:39:01 -0300

    feat: cria guard de autenticação

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,21 +1,15 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   canActivate() {
-    if (1 > 2) {
+    if (this.authService.isAuthenticated()) {
       return true;
     } else {
       this.router.navigate(['/auth/signin']);
