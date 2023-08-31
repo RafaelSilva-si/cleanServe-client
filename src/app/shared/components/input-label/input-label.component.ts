67ecea5d0d86b6e42d5156c9e7ae76ea49fb1595
RafaelSilva-si <rafael.silva@digi.ag>
2023-08-31 08:40:10 -0300

    refactor: ajusta component de input

--- src/app/shared/components/input-label/input-label.component.ts
+++ src/app/shared/components/input-label/input-label.component.ts
@@ -1,15 +1,47 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, forwardRef } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-input-label',
   templateUrl: './input-label.component.html',
   styleUrls: ['./input-label.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputLabelComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputLabelComponent implements OnInit {
+export class InputLabelComponent implements ControlValueAccessor {
   @Input() label: string = 'Label';
+  @Input() formControlName!: string;
   @Input() placeholder: string = 'Placeholder';
+
+  innerValue: any = '';
+
+  onChange: any = () => {};
 
   constructor() {}
 
-  ngOnInit(): void {}
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched() {}
+
+  writeValue(value: any): void {
+    if (value !== undefined) {
+      this.innerValue = value;
+    }
+  }
+
+  updateValue(event: any): void {
+    this.innerValue = event.target.value;
+    this.onChange(this.innerValue);
+  }
 }
