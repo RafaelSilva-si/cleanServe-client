6cc5115fc72eefe9c1db4078e79aa00b1f13cf3d
RafaelSilva-si <rafael.silva@digi.ag>
2023-08-31 08:40:47 -0300

    feat: adiciona guard no app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,31 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SharedModule } from './shared/shared.module';
 import { AuthComponent } from './modules/layout/auth/auth.component';
 import { AuthenticatedComponent } from './modules/layout/authenticated/authenticated.component';
+import { HttpClientModule } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+export function tokenGetter() {
+  return localStorage.getItem('access_token');
+}
 
 @NgModule({
   declarations: [AppComponent, AuthenticatedComponent, AuthComponent],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    SharedModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+      },
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
